d80164b20dc2f8ef2086854a89ca27100d47731d
jj008 <50169669+jj008@users.noreply.github.com>
2020-10-17 12:00:43 +0000

    all 5 templates migrations

--- Geekz_Microschool/settings.py
+++ Geekz_Microschool/settings.py
@@ -198,7 +198,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER='unknownmailleruser@gmail.com'
-EMAIL_HOST_PASSWORD='Abdce@54321'
+EMAIL_HOST_PASSWORD='password'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ACCOUNT_FORMS = {'signup': 'home.forms.SimpleSignupForm'}
