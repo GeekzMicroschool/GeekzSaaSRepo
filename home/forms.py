da5533b2f0e92746a89c2c9b2a297d0697221cfc
jj008 <50169669+jj008@users.noreply.github.com>
2020-10-18 17:32:24 +0000

    custom email template for verification and welcome

--- home/forms.py
+++ home/forms.py
@@ -6,6 +6,12 @@ from django.core.validators import FileExtensionValidator
 
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
 
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+
 class SimpleSignupForm(SignupForm):
     email = forms.EmailField(max_length=255)
     fullname = forms.CharField(max_length=200)
@@ -48,4 +54,13 @@ class CustomSocialSignupForm(SocialSignupForm):
                 whatsapp_no = ''
         fullname = user.first_name+" "+user.last_name
         UserDetails.objects.create(email = user.email, fullname = fullname, phone_no = self.cleaned_data['phone_no'], whatsapp_no = whatsapp_no)
+
+        #welcome mail
+        subject='Welcome to Geekz!'
+        html_template='socialaccount/email/welcome_email.html'
+        html_message=render_to_string(html_template,{'user':user.first_name})
+        to_email=user.email
+        message=EmailMessage(subject, html_message, settings.EMAIL_HOST_USER, [to_email])
+        message.content_subtype='html'
+        message.send()
         return user
