9b28b2433ddd2d4ea5debdf7ee341747406b4b21
Sundar Ponnusamy <40369754+svnsundar@users.noreply.github.com>
2020-10-30 20:50:19 -0400

    Revert "Dev docker"

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('schoolasaservice', views.schoolasaservice, name="schoolasaservice"),
     path('homeschool', views.homeschool, name="homeschool"),
+    path('apply', views.apply, name="apply"),
     path('jobs', views.jobs, name="jobs"),
-    path('logout', views.logout, name="logout"),
-    #path('gsignup', views.gsignup, name="gsignup"),
-    #path('glogin', views.glogin, name="glogin"),
-    #path('login', views.login, name="login"),
-    #path('signup', views.signup, name="signup"),
 ]
--- schoolasaservice/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SchoolasaserviceConfig(AppConfig):
-    name = 'schoolasaservice'
