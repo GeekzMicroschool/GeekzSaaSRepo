193c07d9e3a2956bfeeb1dc7f3d836c3f47e8da2
Sundar Ponnusamy <svnsundar@gmail.com>
2020-11-02 13:23:47 +0000

    dokcer changes to application branch

---
+++ docker-gunicorn.conf.py
@@ -0,0 +1,6 @@
+accesslog = "/home/geekzsaas/sundar/GeekzSaaS/access.log"
+errorlog = "/home/geekzsaas/sundar/GeekzSaaS/error.log"
+#--workers=2 --threads=4 --worker-class=gthread 
+#import multiprocessing
+
+#workers = multiprocessing.cpu_count() * 2 + 1
